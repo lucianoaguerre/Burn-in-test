89261936f3b1ae0bbcc83246d5c311af1dabdca4
lucianoaguerre <luchoaguerre@gmail.com>
2021-06-29 07:19:52 -0300

    Hardware final version

--- Firmware/test-module/test-module.ino
+++ Firmware/test-module/test-module.ino
@@ -7,44 +7,28 @@
 * Compiler              :   Arduino 1.8.13
 * Target                :   ESP-WROOM-32
 * Notes                 :   None
-*
 *******************************************************************************/
 
-/******************************************************************************
-* 
-* https://randomnerdtutorials.com/esp32-esp-now-wi-fi-web-server/
-* https://randomnerdtutorials.com/esp-now-many-to-one-esp32/
-* 
-*******************************************************************************/
 
 /******************************************************************************
 * Libraries needed
 * *****************************************************************************
-* AsyncHTTPRequest_Generic
-* AsyncTCP
-* ESP_Mail_Client
-* ESP32_Mail_Client
-* ESPAsyncWebServer
-* WifiManager
-* Time - Michael Margolis
-* TimeAlarms - Michael Margolis
-* DS1307RTC - Michael Margolis
-* NTPClient - Fabrice Weinberg
 * 
 *******************************************************************************/
 
 /******************************************************************************
 * Funcional description
-* 
+* El circuito realiza un ciclado para el burn- test de fuentes de alimentacion
+* luego hace un advertizing de los resultados del ensayo a travez de BLE 
 * 
 *******************************************************************************/
 
 /******************************************************************************
 * Includes
 *******************************************************************************/
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <WebServer.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 #include <EEPROM.h>
 #include <rom/rtc.h>
 #include <driver/adc.h>
@@ -67,25 +51,24 @@
 /******************************************************************************
 * Constants Definitions
 *******************************************************************************/
-const char*     ssid = "Default SSID";
-const char*     passphrase = "Default passord";
-//const char* network = "UTORAK_NET";
-const char*     network = "WIFI_TEST";
 
 const float     MIN_VOLT = 10.8;    //12V -10%
 const int       MILS2SEC = 1000;
 const int       INTERVALO_C = 5 * MILS2SEC;     // 5 Segundos
 const int       INTERVALO_L = 60 * MILS2SEC;    // 60 segundos
 
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define         SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define         CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+const char*     DEVICE_NAME = "PIXART-BIT-0001";
+
 /******************************************************************************
 * Variable Definitions
 *******************************************************************************/
-//int             i = 0;
 int             statusCode;
 String          content;
-String          esid;
-String          epass = "";
-WebServer       server(80);
 
 int             cycle_state = 0;
 int             cycle_loop_c = 0;
@@ -93,15 +76,11 @@ int             cycle_loop_l = 0;
 unsigned long   tiempo = 0;
 float           tension_entrada = 0;
 
-
+byte            val = 0;
+BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
 /******************************************************************************
 * Functions Declarations
 *******************************************************************************/
-void wifi_provisioning(void);
-void wifi_provisioning_launchWeb(void);
-void wifi_provisioning_setupAP(void);
-void clear_eeprom(void);
-void read_eeprom(void);
 void hardware_cfg(void);
 void read_adc (void);
 
@@ -111,11 +90,8 @@ void read_adc (void);
 void setup()
 {
   hardware_cfg();
+  BLE_cfg();
   Serial.begin(115200); 
-  //WiFi.disconnect();
-  //EEPROM.begin(512);
-  //delay(10);
-  //wifi_provisioning();
 
   cycle_state = 0;
   Serial.println("Setup terminado");
@@ -136,7 +112,85 @@ void setup()
 *******************************************************************************/
 void loop() 
 {
-      switch (cycle_state)
+     //test_procedure();
+     BLE_send_data();
+     delay(2000);
+}
+
+/******************************************************************************
+* Functions Definitions
+*******************************************************************************/
+void hardware_cfg (void)
+{
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(ACT, OUTPUT);
+  pinMode(VIN, INPUT);
+  
+  digitalWrite(LED_RED,LOW);
+  digitalWrite(LED_GREEN,LOW);
+  digitalWrite(ACT,LOW);
+  
+  adc1_config_width(ADC_WIDTH_BIT_11);
+  adc1_config_channel_atten(ADC1_CHANNEL_4,ADC_ATTEN_DB_11);
+}
+
+void read_adc (void)
+{
+    float factor = 105.315;     // (2^11 bits) / {(33k/5k6) * 3.3v }
+    float adc_tension = 0;
+    int adc_raw = adc1_get_raw(ADC1_CHANNEL_4);
+    adc_tension = (float) adc_raw / factor;  
+    if (tension_entrada == 0)
+    {
+      tension_entrada = adc_tension;
+    }
+    else
+    {
+      tension_entrada = (tension_entrada + adc_tension) / 2;
+    }
+}
+
+void BLE_cfg (void)
+{
+  BLEDevice::init(DEVICE_NAME);
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("data");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT,ESP_PWR_LVL_P9);   
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);      
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN, ESP_PWR_LVL_P9);      
+  
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void BLE_send_data (void)
+{
+  pAdvertising->stop();
+  String _val = String(val++);
+  BLEAdvertisementData data;
+  data.setFlags(0x6);
+  data.setManufacturerData(_val.c_str());
+  pAdvertising->setAdvertisementData(data);
+  pAdvertising->start();
+}
+
+void test_procedure (void)
+{
+  switch (cycle_state)
       {
         case 0:
               read_adc();
@@ -253,157 +307,4 @@ void loop()
       }
 
 }
-
-/******************************************************************************
-* Functions Definitions
-*******************************************************************************/
-void hardware_cfg (void)
-{
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(ACT, OUTPUT);
-  pinMode(VIN, INPUT);
-  
-  digitalWrite(LED_RED,LOW);
-  digitalWrite(LED_GREEN,LOW);
-  digitalWrite(ACT,LOW);
-  
-  adc1_config_width(ADC_WIDTH_BIT_11);
-  adc1_config_channel_atten(ADC1_CHANNEL_4,ADC_ATTEN_DB_11);
-}
-
-void read_adc (void)
-{
-    float factor = 105.315;     // (2^11 bits) / {(33k/5k6) * 3.3v }
-    float adc_tension = 0;
-    int adc_raw = adc1_get_raw(ADC1_CHANNEL_4);
-    adc_tension = (float) adc_raw / factor;  
-    if (tension_entrada == 0)
-    {
-      tension_entrada = adc_tension;
-    }
-    else
-    {
-      tension_entrada = (tension_entrada + adc_tension) / 2;
-    }
-}
-
-void clear_eeprom (void)
-{
-  Serial.println("clearing eeprom");
-  for (int i = 0; i < 96; ++i) 
-  {
-    EEPROM.write(i, 0);
-  }
-}
-
-void read_eeprom (void)
-{
-  for (int i = 0; i < 32; ++i)
-  {
-    esid += char(EEPROM.read(i));
-  } 
-  for (int i = 32; i < 96; ++i)
-  {
-    epass += char(EEPROM.read(i));
-  }
-}
-        
-void wifi_provisioning (void)
-{
-  read_eeprom();
-  WiFi.begin(esid.c_str(), epass.c_str());
-  for (int i = 0; i < 10; ++i)
-  {
-    delay(1000);
-  } 
-  
-  if ((WiFi.status() == WL_CONNECTED))
-  {
-      Serial.print("Connected to ");
-      Serial.print(esid);
-      Serial.println(" Successfully");
-      delay(100);
-  }
-  else
-  {
-    Serial.println("Turning the HotSpot On");
-    wifi_provisioning_launchWeb();
-    wifi_provisioning_setupAP();// Setup HotSpot
-  }
-  while ((WiFi.status() != WL_CONNECTED))
-  {
-    delay(100);
-    server.handleClient();
-  }
-}
-
-void wifi_provisioning_launchWeb()
-{
-  wifi_provisioning_createWebServer();
-  server.begin();
-}
-
-void wifi_provisioning_setupAP(void)
-{
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-  WiFi.softAP(network, "");
-  Serial.println("Initializing PIXART_NET WiFi");
-  wifi_provisioning_launchWeb();
-}
-
-void wifi_provisioning_createWebServer()
-{
-  {
-    server.on("/", []() 
-    {
-      IPAddress ip = WiFi.softAPIP();
-      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-      content = "<!DOCTYPE HTML>\r\n<html>PIXART Burn-In Test - Configuracion del WiFi";
-      content += "<p> Ingrese los datos de su WiFi";
-      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32> <label>Password: </label><input name='pass' length=64><input type='submit'></form>";
-      content += "</html>";
-      server.send(200, "text/html", content);
-    });
-    
-    server.on("/setting", []() 
-    {
-      String qsid = server.arg("ssid");
-      String qpass = server.arg("pass");
-      if (qsid.length() > 0 && qpass.length() > 0) 
-      {
-        clear_eeprom();
-        for (int i = 0; i < qsid.length(); ++i)
-        {
-          EEPROM.write(i, qsid[i]);
-        }
-        Serial.println("writing eeprom pass:");
-        for (int i = 0; i < qpass.length(); ++i)
-        {
-          EEPROM.write(32 + i, qpass[i]);
-        }
-        EEPROM.commit();
-        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
-        statusCode = 200;
-        ESP.restart();
-      } 
-      else 
-      { 
-        content = "{\"Error\":\"404 not found\"}";
-        statusCode = 404;
-        Serial.println("Sending 404");
-      }
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-      server.send(statusCode, "application/json", content);
-    });
-  }
-}
-
-
-
-
-
-
 //**** END OF FILE **********************************************************************
